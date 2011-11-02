195361b1f0c46eba0a27b4224f157eb331ab48c2
Chris Ostrowski <chris.ostrowski@earlyaprilmedia.com>
2011-11-02 00:25:09 -0400

    Generated new Rails 3.1.1 app with Spine.JS + Eco Gems.

---
+++ lax_house/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaxHouse::Application.config.secret_token = 'ddfd6f387afed7bb5a3c5348e06ae8d20347c1bec32034c0dc6ae3b16e8623cb1404e37ecaac0f940d3e596b624e6c8caded081bcca80572ecbc801f3d4fa949'
